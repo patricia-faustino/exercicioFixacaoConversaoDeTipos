b3bde2731763696526ed9f4799d1233c448ef070
Patricia <patriciafaustinobr@gmail.com>
2020-09-30 19:02:30 -0300

    Fiz exercicios de fixacao de conversao de tipos e comandos de entrada

---
+++ exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite dois valores inteiros");
+            int a, b, soma;
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+             soma =a+b;
+            Console.WriteLine("SOMA: "+soma);
+        }
+    }
+}
